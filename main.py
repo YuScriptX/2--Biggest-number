0db956ae466dfa93924c763616f56a08b57806cd
YuScriptX <yusuf.tiftik@outlook.com>
2025-10-31 09:42:43 +0100

    add program to find the biggest of two numbers

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Biggest Number
+
+a = int(input("Enter a number?: "))
+b = int(input("Enter a number?: "))
+
+def max_of_two():
+    if a > b:
+        return a
+    else:
+        return b
+    
+print(max_of_two())
